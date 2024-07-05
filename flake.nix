e9aa8a0ef1521a5feba5f55150dc43cdc9e631e7
Matias Yocca <matias@yocca.dev>
2024-07-05 20:11:50 -0300

    refactor: improve flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,24 @@
 {
   description = "Fleeca Home Banking";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-24.05";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, ... }:
     let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          jdk
-        ];
-      };
-
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        name = "fleeca";
-        src = ./.;
-
-        buildInputs = [ pkgs.jdk ];
-
-        buildPhase = ''
-          mkdir -p classes
-          find src -name "*.java" > sources.txt
-          javac -d classes @sources.txt
-        '';
-
-        installPhase = ''
-          mkdir -p $out/bin
-          cd classes
-          jar cfm $out/bin/fleeca.jar ../manifest.mf $(find . -name "*.class")
-        '';
-
-        meta = with pkgs.lib; {
-          description = "A simple Java project built with Nix flakes";
-          license = licenses.mit;
-          maintainers = [ maintainers.mayocca ];
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk
+          ];
         };
-      };
+      });
     };
 }
