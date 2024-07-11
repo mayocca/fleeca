73224e7416c62e4706294f0cfb34acc96ddb739d
Matias Yocca <matias@yocca.dev>
2024-07-10 22:04:29 -0300

    refactor: update build configuration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fleeca Home Banking";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/release-24.05";
+    nixpkgs.url = "nixpkgs/24.05";
   };
 
   outputs = { nixpkgs, ... }:
@@ -12,12 +12,46 @@
         pkgs = import nixpkgs { inherit system; };
       });
     in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+    rec {
+      devShells = forEachSupportedSystem ({ pkgs, ... }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
+            ant
             jdk
           ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs, ... }: {
+        default = pkgs.stdenv.mkDerivation {
+          pname = "fleeca";
+          version = "1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            ant
+            jdk
+            stripJavaArchivesHook
+          ];
+
+          buildPhase = ''
+            runHook preBuild
+            ant clean
+            ant dist
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm644 dist/fleeca.jar $out/share/java/fleeca.jar
+            runHook postInstall
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Fleeca Home Banking";
+            license = licenses.mit;
+          };
         };
       });
     };
