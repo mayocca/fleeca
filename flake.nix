ecac46f077466df37c7f9ce407129edd665148b2
Matias Yocca <matias@yocca.dev>
2025-04-25 17:03:11 -0300

    chore: add initial nix configuration and build setup

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        fleeca = pkgs.stdenv.mkDerivation {
+          name = "fleeca";
+          version = "1.0.0";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            ant
+            jdk
+            makeWrapper
+            stripJavaArchivesHook
+          ];
+
+          buildPhase = ''
+            runHook preBuild
+            ant
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm644 dist/fleeca.jar $out/share/java/fleeca.jar
+            makeWrapper ${pkgs.jdk}/bin/java $out/bin/fleeca \
+              --add-flags "-jar $out/share/java/fleeca.jar"
+            runHook postInstall
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A simple bank application";
+            homepage = "https://github.com/mayocca/fleeca";
+            licenses = with licenses; [
+              agpl3Only
+            ];
+            platforms = platforms.all;
+          };
+        };
+      in
+      rec {
+        # --- packages ---
+        packages.fleeca = fleeca;
+        packages.default = packages.fleeca;
+
+        # --- apps ---
+        apps.fleeca = flake-utils.lib.mkApp { drv = packages.fleeca; };
+        apps.default = apps.fleeca;
+
+        # --- formatter ---
+        formatter = pkgs.nixfmt-rfc-style;
+
+        # --- shell ---
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ant
+            jdk
+          ];
+        };
+      }
+    );
+}
